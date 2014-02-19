5bd3e9468cc0b7294e1bb6f40c242f3d1bbd6f8a
git <cedric.geissmann@gmail.com>
2014-02-20 00:48:10 +0100

    test revert

--- clean-up/index.php
+++ clean-up/index.php
@@ -33,7 +33,7 @@
 							<button id="loginButton" type="submit" class="btn btn-primary">Anmelden</button>
 					</div>
 					<div class="form-center">
-							ode <a id="signupLink" href="signup.php">Einschreiben</button>
+							oder <a id="signupLink" href="signup.php">Einschreiben</button>
 					</div>
 				</form>
 			</div>
